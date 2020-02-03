7ef217bc4385b43775015b9c3a6feb6bf7bde50d
Wordpress-Webmaster <60612681+Wordpress-Webmaster@users.noreply.github.com>
2020-02-03 22:23:10 +0100

    Update class-wps-wwis.php

--- includes/class-wps-wwis.php
+++ includes/class-wps-wwis.php
@@ -41,6 +41,7 @@ class Wps_Wwis {
 	}
 	private function define_admin_hooks() {
 		$plugin_admin = new Wps_Wwis_Admin( $this->get_plugin_name(), $this->get_version() );
+		$this->loader->add_action( 'admin_menu', $plugin_admin, 'wpspeed_add_admin_menu' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 	}
