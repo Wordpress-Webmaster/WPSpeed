c117a34b64b6ee08d696bd91c932061951fba34e
Wordpress-Webmaster <60612681+Wordpress-Webmaster@users.noreply.github.com>
2020-02-03 16:59:22 +0100

    Add files via upload

---
+++ public/wps-wwis-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://wordpress-webmaster.de
+ * @since      1.0.0
+ *
+ * @package    Wps_Wwis
+ * @subpackage Wps_Wwis/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
