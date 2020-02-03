59bc21e92698e032ca484df7065b4656e7f1b267
Wordpress-Webmaster <60612681+Wordpress-Webmaster@users.noreply.github.com>
2020-02-03 22:55:00 +0100

    Update wps-wwis-admin.js

--- admin/js/wps-wwis-admin.js
+++ admin/js/wps-wwis-admin.js
@@ -1,32 +1,20 @@
-(function( $ ) {
-	'use strict';
-
-	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-
-})( jQuery );
+jQuery(document).ready(function() {
+jQuery('#wpspeed-default-button').click( function(e){
+// Input Checkbox
+jQuery("input[type='checkbox']").each( function($i) {
+jQuery(this).prop('checked', false);
+});
+jQuery("input[type='checkbox'][data-default='true']").each( function($i) {
+jQuery(this).prop('checked', true);
+});
+// Input TEXT
+jQuery("input[type='text']").each( function($i) {
+dataDefault = jQuery(this).attr('data-default');
+if ( "" == dataDefault ){
+jQuery(this).val("");
+} else {
+jQuery(this).val( dataDefault );
+}
+});
+});
+});
