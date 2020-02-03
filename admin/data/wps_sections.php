90e6c48a5da6967a056c85abff7faed8f96e42a8
Wordpress-Webmaster <60612681+Wordpress-Webmaster@users.noreply.github.com>
2020-02-04 00:02:44 +0100

    Update wps_sections.php

--- admin/data/wps_sections.php
+++ admin/data/wps_sections.php
@@ -1,1 +1,34 @@
+<?php
+global $wps_sections;
 
+/**
+ * All sections list
+ * @var array
+ */
+$wps_sections = array(
+	'security'	=> array(
+		'name'			=> __( 'Security', WPS_WWIS_LG ),
+		'description'	=> __( '', WPS_WWIS_LG ),
+		'file'			=> WPS_WWIS_DIR . '/inc/common/actions_security.php',
+	),
+	'media'	=> array(
+		'name'			=> __( 'Media', WPT_LG ),
+		'description'	=> __( '', WPS_WWIS_LG ),
+		'file'			=> WPS_WWIS_DIR . '/inc/common/actions_media.php',
+	),
+	'misc'	=> array(
+		'name'			=> __( 'Misc', WPS_WWIS_LG ),
+		'description'	=> __( '', WPS_WWIS_LG ),
+		'file'			=> WPS_WWIS_DIR . '/inc/common/actions_misc.php',
+	),
+	'gutenberg' => array(
+		'name'			=> __( 'Gutenberg', WPS_WWIS_LG ),
+		'description'	=> __( '', WPS_WWIS_LG ),
+		'file'			=> WPS_WWIS_DIR . '/inc/common/actions_gutenberg.php',
+	),
+	'head'	=> array(
+		'name'			=> __( 'Head', WPS_WWIS_LG ),
+		'description'	=> __( '', WPS_WWIS_LG ),
+		'file'			=> WPS_WWIS_DIR . '/inc/common/actions_head.php',
+	),
+);
