4c5ac344e88999fb4d1e64cd6b2add53ef911c18
Wordpress-Webmaster <60612681+Wordpress-Webmaster@users.noreply.github.com>
2020-02-03 21:48:49 +0100

    Update wps-wwis.php

--- wps-wwis.php
+++ wps-wwis.php
@@ -1,12 +1,8 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
+ * The plugin is under developing it is not finised!
+ * By Volkan Sah Kücükbudak
  *
  * @link              https://wordpress-webmaster.de
  * @since             1.0.0
@@ -19,8 +15,8 @@
  * Version:           1.0.0
  * Author:            Volkan Sah
  * Author URI:        https://wordpress-webmaster.de
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:           CC + GPL + Privat
+ * License URI:       http://soon
  * Text Domain:       wps-wwis
  * Domain Path:       /languages
  */
@@ -29,12 +25,6 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
-
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
 define( 'WPS_WWIS_VERSION', '1.0.0' );
 
 /**
@@ -45,38 +35,15 @@ function activate_wps_wwis() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wps-wwis-activator.php';
 	Wps_Wwis_Activator::activate();
 }
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-wps-wwis-deactivator.php
- */
 function deactivate_wps_wwis() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wps-wwis-deactivator.php';
 	Wps_Wwis_Deactivator::deactivate();
 }
-
 register_activation_hook( __FILE__, 'activate_wps_wwis' );
 register_deactivation_hook( __FILE__, 'deactivate_wps_wwis' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
 require plugin_dir_path( __FILE__ ) . 'includes/class-wps-wwis.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
 function run_wps_wwis() {
-
 	$plugin = new Wps_Wwis();
 	$plugin->run();
-
 }
 run_wps_wwis();
