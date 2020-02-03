28f70f1d3bfd6644c55669f213ce6a248d5991dc
Wordpress-Webmaster <60612681+Wordpress-Webmaster@users.noreply.github.com>
2020-02-03 22:52:03 +0100

    Update class-wps-wwis.php

--- includes/class-wps-wwis.php
+++ includes/class-wps-wwis.php
@@ -14,66 +14,61 @@ class Wps_Wwis {
 		if ( defined( 'WPS_WWIS_VERSION' ) ) {
 			$this->version = WPS_WWIS_VERSION;
 		} else {
-			$this->version = '1.0.0';
+		$this->version = '1.0.0'; 
 		}
 		$this->plugin_name = 'wps-wwis';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		// Workspace 
+		$this->define_core_security();
+		$this->define_core_api();
+		$this->define_public_misc();
 		$this->define_public_hooks();
-
-		$this->define_public_hooks();
-
-	}
-	private function load_dependencies() {
+		}
+		private function load_dependencies() {
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wps-wwis-loader.php';
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wps-wwis-i18n.php';
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-wps-wwis-admin.php';
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-wps-wwis-public.php';
-			$this->loader = new Wps_Wwis_Loader();
-
-	}
-
-	private function set_locale() {
+		$this->loader = new Wps_Wwis_Loader();
+		}
+		private function set_locale() {
 		$plugin_i18n = new Wps_Wwis_i18n();
 		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
-	}
-	private function define_admin_hooks() {
+		}
+		private function define_admin_hooks() {
 		$plugin_admin = new Wps_Wwis_Admin( $this->get_plugin_name(), $this->get_version() );
 		$this->loader->add_action( 'admin_menu', $plugin_admin, 'wpspeed_add_admin_menu' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 		$this->loader->add_action( 'admin_init', $plugin_admin, 'wpspeed_settings_init' );
-	}
-	private function define_public_hooks() {
+		}
+		private function define_core_security() {
+		$plugin_public = new Wps_Wwis_Core_Security( $this->get_plugin_name(), $this->get_version() );
+		$this->
+		}
+		private function define_core_api() {
+		$plugin_public = new Wps_Wwis_Core_Api( $this->get_plugin_name(), $this->get_version() );
+		$this->
+		}
+		private function define_public_hooks() {
 		$plugin_public = new Wps_Wwis_Public( $this->get_plugin_name(), $this->get_version() );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public function run() {
-		$this->loader->run();
-	}
-	public function get_plugin_name() {
-		return $this->plugin_name;
-	}
-	public function get_loader() {
-		return $this->loader;
-	}
-	public function get_version() {
+		}
+		private function define_public_misc() {
+		$plugin_public = new Wps_Wwis_Public_Misc( $this->get_plugin_name(), $this->get_version() );
+		$this->
+		}
+		public function run() {
+				$this->loader->run();
+				}
+		public function get_plugin_name() {
+			return $this->plugin_name;
+				}
+		public function get_loader() {
+			return $this->loader;
+				}
+		public function get_version() {
 		return $this->version;
-	}
-
+				}
 }
